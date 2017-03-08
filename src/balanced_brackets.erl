ad6be3f94060f98749df0e48f39e8de6ab6b3ea7
Alex Moore <moore.alex@gmail.com>
2017-03-08 11:49:01 -0500

    Refactor bracket checking into functions

--- src/balanced_brackets.erl
+++ src/balanced_brackets.erl
@@ -18,20 +18,34 @@ check_balance([], Seen) ->
     length(Seen) == 0;
 
 check_balance(_Input = [Next | T], Seen) ->
-    if
-        Next == $(; Next == ${; Next == $[ ->
-            check_balance(T, [Next | Seen]);
-
-        Next == $) -> check_closer($(, T, Seen);
-        Next == $} -> check_closer(${, T, Seen);
-        Next == $] -> check_closer($[, T, Seen);
-        true -> check_balance(T, Seen)
+    case {is_opener(Next), is_closer(Next)} of
+        {true, false} -> check_balance(T, [Next | Seen]);
+        {false, true} -> check_closer(Next, T, Seen);
+        _Else -> check_balance(T, Seen)
     end.
 
-check_closer(OpeningBracket, InputTail, Seen) ->
+-spec check_closer(char(), string(), [char()]) -> boolean().
+check_closer(ClosingBracket, InputTail, Seen) ->
+    OpeningBracket = get_matching_opener(ClosingBracket),
     case Seen of
         [OpeningBracket | SeenTail] -> check_balance(InputTail, SeenTail);
         _Else -> false
+    end.
+
+-spec is_opener(char()) -> boolean().
+is_opener(Char) ->
+    Char == $( orelse Char == ${ orelse Char == $[.
+
+-spec is_closer(char()) -> boolean().
+is_closer(Char) ->
+    Char == $) orelse Char == $} orelse Char == $].
+
+-spec get_matching_opener(char()) -> char().
+get_matching_opener(Char) ->
+    case Char of
+        $) -> $(;
+        $} -> ${;
+        $] -> $[
     end.
 
 %%% Tests
